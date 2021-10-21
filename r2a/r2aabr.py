3c978526492d5f6f3fabc11689a9956e28d7eefb
Bernardo Ferreira Santos Ximbre <33574741+BernardoXimbre@users.noreply.github.com>
2021-10-21 20:08:06 -0300

    Update r2aabr.py
    implementacao handle

--- r2a/r2aabr.py
+++ r2a/r2aabr.py
@@ -9,3 +9,13 @@ class R2AAbr(IR2A):
         IR2A.__init__(self, id)
         self.parsed_mpd = ''
         self.qi = []
+   
+    def handle_xml_request(self, msg):        
+        self.send_down(msg)
+
+    def handle_xml_response(self, msg):
+        # getting qi list
+        self.parsed_mpd = parse_mpd(msg.get_payload())
+        self.qi = self.parsed_mpd.get_qi()
+
+        self.send_up(msg)
